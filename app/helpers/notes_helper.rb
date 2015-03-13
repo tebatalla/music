303e2d19fb120f131b941fd8843426ca8fbc4a95
Tomas Batalla <tebatalla@gmail.com>
2015-03-12 17:55:34 -0700

    Ugly lyrics helper method

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,9 @@
 module NotesHelper
+  def ugly_lyrics(lyrics)
+    lines = h(lyrics).split("\n")
+    lines.map! do |line|
+      "&#9835; " + line
+    end
+    "<pre>#{lines.join("\n")}</pre>".html_safe
+  end
 end
